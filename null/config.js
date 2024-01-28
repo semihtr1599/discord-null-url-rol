ab8086fbc57f73cd30d7b145672e1ed92ea8522f
semihtr1599 <139440528+semihtr1599@users.noreply.github.com>
2024-01-28 16:50:31 +0300

    Update config.js

--- null/config.js
+++ null/config.js
@@ -3,7 +3,7 @@ module.exports = {
  "TOKEN": 'MTIwMDc0NzQwNzk2NTY5MTk3NA.GtE2io.DFu_6Y9T93yx_lPyrZPr3Wnc3an6WO7H6IhbyUY', // Bot Tokeni.
  "GUILD_ID": '1199723340584198296',  // Sunucu (guild) ID'si.
  "ROLE_ID": '1201128405832323102',    // Hedef rol ID'si.
- "EXPECTED_STATUS": 'Bedeva Hesaplar:https://discord.gg/Fgwee33ttd', // Kişilerin durumu.
+ "EXPECTED_STATUS": 'Bedeva Hesaplar:https://discord.gg/AFQepyYEsp', // Kişilerin durumu.
  "CHANNEL_ID": '1201128285472555048', // Resim kanalı ID'si.
  "EMOJI": '1201127557349789777', // Resimlerin altına eklenecek emoji ID si.
  "STATUS": [
