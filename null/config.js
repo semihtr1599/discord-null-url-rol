4a786e2cc7231ce5cfdb4492c919de95e8d7e902
semihtr1599 <139440528+semihtr1599@users.noreply.github.com>
2024-01-28 16:10:17 +0300

    rol
    rol

--- null/config.js
+++ null/config.js
@@ -1,17 +1,17 @@
 module.exports = {
 
- "TOKEN": '', // Bot Tokeni.
- "GUILD_ID": '',  // Sunucu (guild) ID'si.
- "ROLE_ID": '',    // Hedef rol ID'si.
- "EXPECTED_STATUS": 'themis', // Kişilerin durumu.
- "CHANNEL_ID": '', // Resim kanalı ID'si.
- "EMOJI": '', // Resimlerin altına eklenecek emoji ID si.
+ "TOKEN": 'MTIwMDc0NzQwNzk2NTY5MTk3NA.GOqHbT.dGd4gKElJdllpa6AA68C_XAYt2UjkX5_z1VIzo', // Bot Tokeni.
+ "GUILD_ID": '1199723340584198296',  // Sunucu (guild) ID'si.
+ "ROLE_ID": '1201128405832323102',    // Hedef rol ID'si.
+ "EXPECTED_STATUS": 'Bedeva Hesaplar:https://discord.gg/JYuydubqQB', // Kişilerin durumu.
+ "CHANNEL_ID": '1201128285472555048', // Resim kanalı ID'si.
+ "EMOJI": '1201127557349789777', // Resimlerin altına eklenecek emoji ID si.
  "STATUS": [
-    "null 💛 Themis",
-    "null 💚 Themis",
-    "null 💜 Themis",
-    "null 💓 Themis",
-    "null 💙 Themis"
+    "By 💛 Semih",
+    "By 💚 Semih",
+    "By 💜 Semih",
+    "BY 💓 Semih",
+    "By 💙 Semih"
 ] // Botun oynuyor kısmı.
  
   };
